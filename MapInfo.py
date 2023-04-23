cd148fcf5133486038d6d461ef2e2e641e70e73e
Xcoder <GamingX@posteo.de>
2023-04-23 19:18:16 +0200

    Update MapInfo.py

--- MapInfo.py
+++ MapInfo.py
@@ -498,11 +498,17 @@ win.mainloop()
 #        print("[***] Keine Drei-Stern-Frage zu diesem Land gefunden")
 #    print()
 
+# Example file showing a circle moving on screen
+import pygame
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -514,11 +520,24 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(144)  # limits FPS to 60
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
