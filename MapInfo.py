aec72dfec6e07efeb8e464eb2da1d885d0582f07
Xcoder <GamingX@posteo.de>
2023-04-24 16:11:20 +0200

    Update MapInfo.py

--- MapInfo.py
+++ MapInfo.py
@@ -507,6 +507,7 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+gravity = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -520,17 +521,19 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
+    gravity += 1
+
     pygame.draw.circle(screen, "red", player_pos, 40)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+    if keys[pygame.K_SPACE]:
+        gravity = -10
     if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+    
+    player_pos.y += gravity
 
     # flip() the display to put your work on screen
     pygame.display.flip()
