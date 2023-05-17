8ed6cbd66c17979bcb15c042a7a7b7a9fed8d63e
XGamixX <92629959+XGamixX@users.noreply.github.com>
2023-05-17 11:51:52 +0200

    Update MapInfo.py

--- MapInfo.py
+++ MapInfo.py
@@ -8,6 +8,7 @@ pip install numpy
 pip install pip install emoji-country-flag
 pip install pygame
 pip install pygame_widgets
+pip install google-books-api-wrapper ##intgrate this
 """ ##installation der libraries
 
 from countryinfo import CountryInfo
