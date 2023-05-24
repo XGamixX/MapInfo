a94d78969aba60d7e13f736168b97da6f8ffa54f
Xcoder <GamingX@posteo.de>
2023-05-24 20:35:59 +0200

    Update BookInfo.py

--- BookInfo.py
+++ BookInfo.py
@@ -32,16 +32,16 @@ def append_3 (bookNR, question, correctAnswer, wrongAnswer1, wrongAnswer2, wrong
     global questions_3
     questions_3[bookNR].append([question, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4])
 
-def findWrongAuthor (correctAuthor):
+def findWrongItem (correctItem, ID):
     try:
-        WrongAuthor = (books[randrange(1, int(len(books))) - 1][2])
+        WrongItem = (books[randrange(1, int(len(books))) - 1][ID])
     except:
-        WrongAuthor = findWrongAuthor(correctAuthor)
+        WrongItem = findWrongItem(correctItem)
 
-    if WrongAuthor == correctAuthor:
-        WrongAuthor = findWrongAuthor(correctAuthor)
+    if WrongItem == correctItem:
+        WrongItem = findWrongItem(correctItem)
 
-    return(WrongAuthor)
+    return(WrongAItem)
 
 for bookNR, book in enumerate(books):
     BookName = book[1]
@@ -52,12 +52,36 @@ for bookNR, book in enumerate(books):
     #2: publisher [11]
     #3: page count [7], publish date [10]
     correctAnswer_author = book[2]
-    WrongAuthor1 = findWrongAuthor(correctAnswer_author)
-    WrongAuthor2 = findWrongAuthor(correctAnswer_author)
-    WrongAuthor3 = findWrongAuthor(correctAnswer_author)
-    WrongAuthor4 = findWrongAuthor(correctAnswer_author)
-    question = 'Wer hat "' + BookName + '" geschrieben?'
-    append_1(bookNR, question, correctAnswer_author, WrongAuthor1, WrongAuthor2, WrongAuthor3, WrongAuthor4)
+    WrongAuthor1 = findWrongItem(correctAnswer_author, 2)
+    WrongAuthor2 = findWrongItem(correctAnswer_author, 2)
+    WrongAuthor3 = findWrongItem(correctAnswer_author, 2)
+    WrongAuthor4 = findWrongItem(correctAnswer_author, 2)
+    question_author = 'Wer hat "' + BookName + '" geschrieben?'
+    append_1(bookNR, question_author, correctAnswer_author, WrongAuthor1, WrongAuthor2, WrongAuthor3, WrongAuthor4)
+
+    correctAnswer_publisher = book[11]
+    WrongPublisher1 = findWrongItem(correctAnswer_publisher, 11)
+    WrongPublisher2 = findWrongItem(correctAnswer_publisher, 11)
+    WrongPublisher3 = findWrongItem(correctAnswer_publisher, 11)
+    WrongPublisher4 = findWrongItem(correctAnswer_publisher, 11)
+    question_publisher = 'Wer hat "' + BookName + '" geschrieben?'
+    append_2(bookNR, question_publisher, correctAnswer_publisher, WrongPublisher1, WrongPublisher2, WrongPublisher3, WrongPublisher4)
+
+    correctAnswer_PageCount = book[7]
+    WrongPageCount1 = findWrongItem(correctAnswer_PageCount, 7)
+    WrongPageCount2 = findWrongItem(correctAnswer_PageCount, 7)
+    WrongPageCount3 = findWrongItem(correctAnswer_PageCount, 7)
+    WrongPageCount4 = findWrongItem(correctAnswer_PageCount, 7)
+    question_pageCount = 'Wer hat "' + BookName + '" geschrieben?'
+    append_3(bookNR, question_pageCount, correctAnswer_PageCount, WrongPageCount1, WrongPageCount2, WrongPageCount3, WrongPageCount4)
+
+    correctAnswer_publishDate = book[10]
+    WrongPublishDate1 = findWrongItem(correctAnswer_publishDate, 10)
+    WrongPublishDate2 = findWrongItem(correctAnswer_publishDate, 10)
+    WrongPublishDate3 = findWrongItem(correctAnswer_publishDate, 10)
+    WrongPublishDate4 = findWrongItem(correctAnswer_publishDate, 10)
+    question_publishDate = 'Wer hat "' + BookName + '" geschrieben?'
+    append_3(bookNR, question_publishDate, correctAnswer_publishDate, WrongPublishDate1, WrongPublishDate2, WrongPublishDate3, WrongPublishDate4)
 
 Parser = argparse.ArgumentParser()
 Parser.add_argument("-f", "--folder", required=True)
