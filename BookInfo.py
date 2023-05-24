09050ea23362aa05a26de0a0e0cd6aace8fb0814
Xcoder <GamingX@posteo.de>
2023-05-24 17:32:12 +0200

    Update BookInfo.py

--- BookInfo.py
+++ BookInfo.py
@@ -12,5 +12,20 @@ with open("books.json", "r", encoding="utf-8") as books_file:
     global books
     books = json.load(books_file)
 
-for book in books:
+def append_1 (bookNR, question, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4):
+    global questions_1
+    questions_1[bookNR].append([question, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4])
+
+def append_2 (bookNR, question, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4):
+    global questions_2
+    questions_2[bookNR].append([question, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4])
+
+def append_3 (bookNR, question, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4):
+    global questions_3
+    questions_3[bookNR].append([question, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4])
+
+for bookNR, book in enumerate(books):
     BookName = book[0]
+    global questions_1
+    global questions_2
+    global questions_3
