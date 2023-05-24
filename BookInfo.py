4369b373ff14aa233d6785046a08e4597c7700bf
Xcoder <GamingX@posteo.de>
2023-05-24 16:28:38 +0200

    Update BookInfo.py

--- BookInfo.py
+++ BookInfo.py
@@ -2,6 +2,12 @@ from deep_translator import GoogleTranslator
 import random
 import json
 
+def randrange(start, end):
+    end += 1
+    if start == end:
+        return(start)
+    return(random.randrange(start, end))
+
 with open("books.json", "r", encoding="utf-8") as books_file:
     global books
     books = json.load(books_file)
