a1e20122bd2d5e9fb282fef21dc8aeae32810935
GamingX <GamingX@posteo.de>
2024-04-10 16:19:45 +0200

    requirements best practice shit

--- libraries.bat
+++ libraries.bat
@@ -1,7 +1,1 @@
-pip install countryinfo
-pip install pycountry
-pip install deep_translator
-pip install numpy
-pip install pip install emoji-country-flag
-pip install pygame
-pip install pygame_widgets
+pip install -r requirements.txt
